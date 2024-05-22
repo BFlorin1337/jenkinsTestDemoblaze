1bfbfff8815253937cc20ea7d9948804026c9071
fbocse <fbocse@griddynamics.com>
2024-05-22 13:51:50 +0300

    changed jenkins file

--- cypress.config.js
+++ cypress.config.js
@@ -6,4 +6,14 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
   },
+  reporter: "cypress-multi-reporters",
+  reporterOptions: {
+    reporterEnabled: "mocha-junit-reporter, mochawesome",
+    mochaJunitReporterReporterOptions: {
+      mochaFile: "cypress/results/junit/results-[hash].xml"
+    },
+    mochawesomeReporterOptions: {
+      reportDir: "cypress/results/mochawesome"
+    }
+  }
 });
