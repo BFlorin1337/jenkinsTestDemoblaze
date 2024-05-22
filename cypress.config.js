720a6b4a3a8d545aa730d725f38f04fc39210d77
fbocse <fbocse@griddynamics.com>
2024-05-22 12:11:00 +0300

    changed jenkins file

--- cypress.config.js
+++ cypress.config.js
@@ -1,31 +1,9 @@
 const { defineConfig } = require("cypress");
-const multiReporters = require("@jesus.guerrero/cypress-multi-reporters");
-const xunitReporter = require("@cypress/xunit");
-const jsonReporter = require("cypress-multi-reporters/lib/reporters/json-reporter");
 
 module.exports = defineConfig({
-  // Other Cypress configuration options...
-
-  // Plugins configuration
-  plugins: [
-    multiReporters({
-      reporters: {
-        xunit: {
-          reporter: xunitReporter,
-          outputFile: "results/xunit.xml",
-        },
-        json: {
-          reporter: jsonReporter,
-          outputFile: "results/json/results.json",
-        },
-      },
-    }),
-  ],
-
-  // End-to-end test configuration
   e2e: {
     setupNodeEvents(on, config) {
-      // Implement node event listeners here
+      // implement node event listeners here
     },
   },
 });
